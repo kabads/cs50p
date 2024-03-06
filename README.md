c35f130f95871cf9a6ebbf8cebf1afa7c425ae90
Adam C <2928943+kabads@users.noreply.github.com>
2024-03-06 17:31:35 +0000

    Adding meal solution

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cs50p
+
+src/bank/bank.py
+src/deep/deep.py
+src/extensions/extensions.py
---
+++ src/meal/meal.py
@@ -0,0 +1,21 @@
+def getTime():
+    return input("What time is it? ")
+
+
+def main():
+    inputTime = getTime()
+    time = convert(inputTime)
+    if time >= 7 and time <= 8:
+        print("breakfast time")
+    elif time >= 12 and time <= 13:
+        print("lunch time")
+    elif time >= 18 and time <= 19:
+        print("dinner time")
+
+
+def convert(time):
+    hour, minute = time.split(':')
+    return float(int(hour) + float(minute) / 60)
+
+if __name__ == '__main__':
+    main()
