faabdf958fdaf09a1411ce11976665843e488cfa
Adam C <2928943+kabads@users.noreply.github.com>
2024-07-13 20:42:59 +0100

    Now throwing an error

--- src/seasons/seasons.py
+++ src/seasons/seasons.py
@@ -1,5 +1,6 @@
 from datetime import date
 import inflect
+import sys
 
 p = inflect.engine()
 
@@ -7,7 +8,7 @@ def get_birthdate(dob):
     try:
         return date.fromisoformat(dob)
     except ValueError:
-        return "Invalid date"
+        sys.exit("Invalid date")
 
 def get_age_in_days(birthdate):
     today = date.today()
@@ -19,15 +20,10 @@ def convert_minutes_to_word_minutes(minutes):
 
 
 def main():
-    # TODO need to check for format YYYY-MM-DD - and sys.exit if not
     birthdate = get_birthdate(input("Date of Birth: "))
     days = get_age_in_days(birthdate)
-    # print(f"Days: {days}")
     hours = days * 24
-    # print(f"Hours: {hours}")
     minutes = hours * 60
-    # print(f"Minutes: {minutes}")
-    # words = p.number_to_words(minutes)
     words = convert_minutes_to_word_minutes(minutes)
     print(words.capitalize() + " minutes")
 
